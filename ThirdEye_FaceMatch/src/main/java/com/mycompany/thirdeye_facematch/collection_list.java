5b69b565d4fde53b47a8788436df91916dc1f702
Parvaiz Sajad <57477042+parvaizsajad@users.noreply.github.com>
2021-10-27 16:08:47 +0530

    first commit

---
+++ ThirdEye_FaceMatch/src/main/java/com/mycompany/thirdeye_facematch/collection_list.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.thirdeye_facematch;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.rekognition.model.Face;
+import com.amazonaws.services.rekognition.model.ListFacesRequest;
+import com.amazonaws.services.rekognition.model.ListFacesResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+
+/**
+ *
+ * @author Akash Sahu
+ */
+public class collection_list {
+    public static final String collectionId = "Records1"; //Collection Name
+
+    public static void main(String[] args) throws Exception {
+      
+      AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.standard().withRegion(Regions.US_WEST_2).build();
+
+      ObjectMapper objectMapper = new ObjectMapper();
+
+      ListFacesResult listFacesResult = null;
+      System.out.println("Faces in collection " + collectionId);
+
+      String paginationToken = null;
+      do {
+         if (listFacesResult != null) {
+            paginationToken = listFacesResult.getNextToken();
+         }
+         
+         ListFacesRequest listFacesRequest = new ListFacesRequest()
+                 .withCollectionId(collectionId)
+                 .withMaxResults(1)
+                 .withNextToken(paginationToken);
+        
+         listFacesResult =  rekognitionClient.listFaces(listFacesRequest);
+         List < Face > faces = listFacesResult.getFaces();
+         for (Face face: faces) {
+            System.out.println(objectMapper.writerWithDefaultPrettyPrinter()
+               .writeValueAsString(face));
+         }
+      } while (listFacesResult != null && listFacesResult.getNextToken() !=
+         null);
+   }
+}
